f9664d7c49eeba7bf439b833c0cde94d70961e38
David Cárdenas <santito500@live.com>
2025-03-05 01:57:28 -0500

    feat: Respawn
    Respawn de Anubis al momento de tocar el agua.

---
+++ AnubisAdventure/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Vector2 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Water"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        StartCoroutine(Respawn(0.3f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        transform.position = startPos;
+    }
+}
