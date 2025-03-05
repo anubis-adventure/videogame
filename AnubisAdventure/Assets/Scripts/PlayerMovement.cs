a4f0a922181ab26e705779f7eda5e339a918ea4e
David Cárdenas <santito500@live.com>
2025-03-05 01:15:48 -0500

    feat: anubis sprites y animaciones
    Sprites para Anubis con sus respectivas animaciones para caída, salto y correr. El idle se está manejando un sprite estático.

--- AnubisAdventure/Assets/Scripts/PlayerMovement.cs
+++ AnubisAdventure/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidBody2d;
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     public float moveSpeed = 3f;
     public float jumpForce = 5f;
@@ -14,29 +15,33 @@ public class PlayerMovement : MonoBehaviour
     {
         rigidBody2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float move = Input.GetAxis("Horizontal");
+        animator.SetFloat("move", move);
 
-        
+        //Assign velocity in Y axis to the fall float for animator.
+        animator.SetFloat("fall", rigidBody2d.velocity.y);
+
         if (move != 0)
         {
             rigidBody2d.velocity = new Vector2(moveSpeed * move, rigidBody2d.velocity.y);
 
-            
+
             spriteRenderer.flipX = (move < 0);
         }
         else
         {
-            
+
             rigidBody2d.velocity = new Vector2(0, rigidBody2d.velocity.y);
         }
 
         // Salto
-        float jump = Input.GetAxis("Jump");
-        if (jump > 0 && CheckGround.isGrounded)
+        bool jump = Input.GetButtonDown("Jump");
+        if (jump && CheckGround.isGrounded)
         {
             rigidBody2d.velocity = new Vector2(rigidBody2d.velocity.x, jumpForce);
         }
