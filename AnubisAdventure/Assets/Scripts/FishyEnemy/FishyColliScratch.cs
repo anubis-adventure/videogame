2d6def808669151a37432b8e5a3a25015e092183
Alero <alejandrorodriguezq0@gmail.com>
2025-04-02 17:36:23 -0500

    the fish'll hurt you, you can kill him now, bubbles particles and some animations

---
+++ AnubisAdventure/Assets/Scripts/FishyEnemy/FishyColliScratch.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishyColliScratch : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Scratch"))
+        {
+            Debug.Log("El gatico ataco el pez");
+
+            if (animator != null)
+            {
+                animator.SetTrigger("die");
+                StartCoroutine(DestroyAfterAnimation());
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    System.Collections.IEnumerator DestroyAfterAnimation()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        Destroy(gameObject);
+    }
+}
