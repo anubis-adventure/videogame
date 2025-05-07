9e8859af23b0b6c5cbe5baa988162f68a7415aac
Sofía princess <sofusaka@gmail.com>
2025-05-07 01:04:00 -0500

    initial Animation

--- AnubisAdventure/Assets/MainMenu.cs
+++ AnubisAdventure/Assets/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync("Scene_one");
     }
 
     public void QuitGame()
